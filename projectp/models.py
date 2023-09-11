7b20cf7ffad45cc954f8b7052bca5c92a41e2574
rdclvanessa <gvvalenzuela1@buffs.wtamu.edu>
2023-09-10 23:49:43 -0500

    Initial commit, Activity 2.01

---
+++ projectp/models.py
@@ -0,0 +1,39 @@
+from django.db import models
+
+
+class Project(models.Model):
+    """Project name and details"""
+    name = models.CharField(
+        max_length=50,
+        help_text="Name of the Project."
+    )
+    lead = models.CharField(
+        max_length=50,
+        help_text="Team lead name"
+    )
+    dateStarted = models.DateField(
+        verbose_name="Date the project was started."
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class Task(models.Model):
+    """Task labels, details and completion"""
+    label = models.CharField(
+        max_length=50,
+        help_text="Label of the task"
+    )
+    description = models.TextField(
+        help_text="Task description"
+    )
+    project = models.ForeignKey(
+        Project, on_delete=models.CASCADE)
+    complete = models.BooleanField(
+        default=False,
+        help_text="Task completed or not"
+    )
+
+    def __str__(self):
+        return self.label
